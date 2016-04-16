20989fe59122a838acc306fe119ba55da7b15f7f
lima9 <max9@openmailbox.org>
2016-04-16 20:42:39 +0200

    Empezada encriptacion en userCtrl.js

--- public/assets/js/controllers/userCtrl.js
+++ public/assets/js/controllers/userCtrl.js
@@ -1,4 +1,4 @@
-cities2.controller('userCtrl',['$scope', '$state','$http','md5', function($scope, $state, $http, md5) {
+cities2.controller('userCtrl',['$scope', '$state','$http','md5', 'paillier', function($scope, $state, $http, md5) {
     $scope.newUser = {};
     // Funcion para crear un usuario
     $scope.addUser = function (newUser) {
@@ -9,23 +9,35 @@ cities2.controller('userCtrl',['$scope', '$state','$http','md5', function($scope
         else if (!newUser.ciudad){
         }
         else{
-            console.log("1: A-->TTP: (TTP, B, M, Po)");
+            console.log("1: A-->TTP: (TTP, B, M, Po) + Lima");
             var ttp="localhost:3000/ttp/adduser";
             var b="localhost:8000/server/adduser";
             var M=newUser;
+            var keys = paillier.generateKeys(512);
+            console.log("This is it: " + keys)
+
             var Mhash=md5.createHash(JSON.stringify(M));
+            var MhashEnc = keys.privateKey.encrypt(Mhash);
+
             var Po={
                 ttp:ttp,
                 b:b,
-                Mhash:Mhash    //deberá ser el HASH de M
+                Mhash:MhashEnc    //deberá ser el HASH de M
             };  //encriptado con la privada de A (Firmado)
             var mensaje ={
                 ttp:ttp,
                 b:b,
                 M:M,    //encriptado con la publica de B
                 Po:Po
-            };  //encriptado con la publica de TTP
-            $http.post('/ttp/adduser', mensaje)
+            };
+            var mensajeString = JSON.stringify(mensaje)
+            var cleartext = new BUffer(mensajeString)
+            var MensPreparado, MensEncr;
+            MensPreparado = BigInteger(cleartext.toString('hex'), 16)
+            MensEncr = keys.publicKey.encrypt(MensPreparado);
+
+            //encriptado con la publica de TTP
+            $http.post('/ttp/adduser', MensEncr)
                 .success(function (data) {
                     $scope.resultado = 'correcto'
                 })
@@ -37,22 +49,41 @@ cities2.controller('userCtrl',['$scope', '$state','$http','md5', function($scope
     
     $scope.AllUsers = function(){
         console.log("1: A-->TTP: (TTP, B, M, Po)");
+        console.log("Estoy aquí")
         var ttp="localhost:3000/ttp/allusers";
         var b="localhost:8000/server/allusers";
-        var M="GET ALL USERS";
+        var M="GET ALL USERS NOW";
+        var keys = paillier.generateKeys(512);
+        console.log("This is it: " + keys)
         var Mhash=md5.createHash(M);
+        var MhashEnc = keys.privateKey.encrypt(Mhash);
+        var MEnc = keys.publicKey.encrypt(M);
         var Po={
             ttp:ttp,
             b:b,
-            Mhash:Mhash     //deberá ser el HASH de M
-        };  //encriptado con la privada de A (Firmado)
+            Mhash:MhashEnc     //deberá ser el HASH de M encriptado con la privada de A (Firmado)
+        };
         var mensaje ={
             ttp:ttp,
             b:b,
-            M:M,    //encriptado con la publica de B
+            M:MEnc,    //encriptado con la publica de B
             Po:Po
-        };  //encriptado con la publica de TTP
-        $http.post('/ttp/allusers', mensaje)
+        };
+        var mensajeString = JSON.stringify(mensaje)
+        var cleartext = new BUffer(mensajeString)
+        var MensPreparado, MensEncr;
+        MensPreparado = BigInteger(cleartext.toString('hex'), 16)
+        MensEncr = keys.publicKey.encrypt(MensPreparado);
+
+
+        /* +  var x = JSON.stringify(Ps);
+         +    var cleartext = new Buffer(x);
+         +    var PsEn, EncryptedPs;
+         +    PsEn = BigInt(cleartext.toString('hex'), 16);
+         +    EncryptedPs = keys.publicKey.encrypt(PsEn);*/
+
+        //encriptado con la publica de TTP
+        $http.post('/ttp/allusers', MensEncr)
             .success(function (data) {
                 $scope.resultado2 = 'correcto';
                 document.getElementById("datosUsers").innerHTML = JSON.stringify(data.data2.L, undefined, 2)
